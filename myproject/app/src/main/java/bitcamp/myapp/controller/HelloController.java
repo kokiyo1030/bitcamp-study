75617fa2cd8fd3fb1f9314d963c1370b4739a138
김태현 <kokiyo1030@naver.com>
2025-02-21 11:50:01 +0900

    chartset 추가

--- myproject/app/src/main/java/bitcamp/myapp/controller/HelloController.java
+++ myproject/app/src/main/java/bitcamp/myapp/controller/HelloController.java
@@ -2,6 +2,7 @@ package bitcamp.myapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,7 +11,8 @@ public class HelloController {
         System.out.println("HelloController 생성됨!");
     }
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", produces = "text/html; charset=utf-8")
+    @ResponseBody
     public String hello() {
         return "<html><body><h1>하하하하</h1></body></html>";
     }
