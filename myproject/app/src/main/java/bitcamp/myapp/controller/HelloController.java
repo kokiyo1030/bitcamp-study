e415120d4d40948a413a07c49e6b6cc613498913
김태현 <kokiyo1030@naver.com>
2025-03-11 18:38:18 +0900

    commit

--- myproject/app/src/main/java/bitcamp/myapp/controller/HelloController.java
+++ myproject/app/src/main/java/bitcamp/myapp/controller/HelloController.java
@@ -1,6 +1,7 @@
 package bitcamp.myapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,7 +12,8 @@ public class HelloController {
         System.out.println("HelloController 생성됨!");
     }
 
-    @RequestMapping(value = "/hello", produces = "text/html; charset=utf-8")
+//    @RequestMapping(value = "/hello", produces = "text/html; charset=utf-8")
+    @GetMapping(value = "/hello", produces = "text/html; charset=utf-8")
     @ResponseBody
     public String hello() {
         return "<html><body><h1>하하하하</h1></body></html>";
