875d0e0ca2b3bb0ce644256108c06889e6ddac81
김태현 <kokiyo1030@naver.com>
2025-04-02 11:39:29 +0900

    jwt

---
+++ myproject/app/src/main/java/bitcamp/myapp/RSAKeyGenerator.java
@@ -0,0 +1,41 @@
+package bitcamp.myapp;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class RSAKeyGenerator {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+
+        // private/public 키 쌍을 생성
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        // private/public 키를 byte[]로 변환
+        byte[] privateKey = keyPair.getPrivate().getEncoded();
+        byte[] publicKey = keyPair.getPublic().getEncoded();
+
+        // byte[] 형식의 RSA 키를 PEM 형식으로 변환
+        String privateKeyPem = convertToPemFormat("PRIVATE KEY", privateKey);
+        String publicKeyPem = convertToPemFormat("PUBLIC KEY", publicKey);
+
+        System.out.println(privateKeyPem);
+        System.out.println(publicKeyPem);
+
+//        System.out.println(Paths.get("private.pem").toAbsolutePath());
+//        System.out.println(Paths.get("public.pem").toAbsolutePath());
+
+        Files.write(Paths.get("private.pem"), privateKeyPem.getBytes());
+        Files.write(Paths.get("public.pem"), publicKeyPem.getBytes());
+    }
+
+    private static String convertToPemFormat(String type, byte[] keyBytes) {
+        String base64Encoded = Base64.getEncoder().encodeToString(keyBytes);
+        return "-----BEGIN " + type + "-----\n" + base64Encoded + "\n-----END " + type + "-----";
+    }
+}
